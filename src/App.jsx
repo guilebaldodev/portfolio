1565b8ce43f16a030da65f9ec2f5d696ee9db9ef
Guilebaldo <guilebaldodev@gmail.com>
2025-06-18 15:03:11 -0600

    v.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,13 @@ function App() {
     },
 
     {
+      name: "Stripe",
+      description: "Gestor de pagos online",
+      alt: "Icono de Stripe",
+      img: "stripe.png",
+    },
+
+    {
       name: "GitHub",
       description: "Plataforma para control de versiones",
       alt: "Icono de GitHub",
@@ -53,12 +60,6 @@ function App() {
       img: "css.png",
     },
     {
-      name: "HTML",
-      description: "Lenguaje de marcado",
-      alt: "Icono de HTML",
-      img: "html.png",
-    },
-    {
       name: "MySQL",
       description: "Base de datos relacional",
       alt: "Icono de MySQL",
@@ -70,17 +71,12 @@ function App() {
       alt: "Icono de MongoDB",
       img: "mongodb.png",
     },
-    {
-      name: "C#",
-      description: "Lenguaje de programación",
-      alt: "Icono de C#",
-      img: "csharp.png",
-    },
   ];
 
-  const message = encodeURIComponent('¡Hola! Me gustaría recibir más información sobre tus servicios.');
+  const message = encodeURIComponent(
+    "¡Hola! Me gustaría recibir más información sobre tus servicios."
+  );
   const whatsAppURL = `https://wa.me/527443112193?text=${message}`;
-
 
   return (
     <>
@@ -90,27 +86,37 @@ function App() {
         <h2 className="title-section">Experiencia</h2>
 
         <div className="experience-list">
-        <div className="experience">
+          <div className="experience">
             <div className="circle-title">
               <img src="/icons/suitcase.png" alt="" />
             </div>
             <div className="experience-info">
               <div className="experience-title">
                 <h4>
-                Desarrollador Full Stack - <span>Freelance</span>{" "}
+                  Desarrollador Full Stack - <span>Freelance</span>{" "}
                 </h4>
               </div>
-              <span >Enero 2024 - Presente</span>
+              <span>Julio 2024 - Presente</span>
 
-                <p className="experience-description">
-
-                Como desarrollador full stack freelance, me encargo de todo el proceso de desarrollo de software, desde el diseño y la gestión de la base de datos, hasta la implementación del frontend y backend, pasando por la integración de APIs y el despliegue en producción.
+              <p className="experience-description">
+                Como desarrollador full stack freelance, me encargo de todo el
+                proceso de desarrollo de software, desde el diseño y la gestión
+                de la base de datos, hasta la implementación del frontend y
+                backend, pasando por la integración de APIs y el despliegue en
+                producción.
                 <br /> <br></br>
-Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL según las necesidades del proyecto. Utilizo Git y GitHub para el control de versiones y colaboraciones, asegurando un flujo de trabajo eficiente. Además, optimizo el rendimiento y la seguridad en cada desarrollo, implementando buenas prácticas en hosting y despliegue en plataformas como Vercel. Mi enfoque está en crear soluciones eficientes y bien estructuradas, adaptándome a los requerimientos de cada cliente y garantizando productos de alta calidad.
-                </p>
+                Trabajo con Next.js, React y Node.js, manejando bases de datos
+                SQL y NoSQL según las necesidades del proyecto. Utilizo Git y
+                GitHub para el control de versiones y colaboraciones, asegurando
+                un flujo de trabajo eficiente. Además, optimizo el rendimiento y
+                la seguridad en cada desarrollo, implementando buenas prácticas
+                en hosting y despliegue en plataformas como Vercel. Mi enfoque
+                está en crear soluciones eficientes y bien estructuradas,
+                adaptándome a los requerimientos de cada cliente y garantizando
+                productos de alta calidad.
+              </p>
             </div>
           </div>
-
 
           <div className="experience">
             <div className="circle-title">
@@ -122,23 +128,19 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
                   Desarrollador Web - <span>Ingeniotics</span>{" "}
                 </h4>
               </div>
-              <span >Enero 2024 - Julio 2024</span>
+              <span>Enero 2024 - Julio 2024</span>
 
-                <p className="experience-description">
-                  Desarrollé Simponsi, un software de
-                  administración y control de artículos en congresos
-                  científicos. Me encargué del{" "}
-                    diseño, desarrollo frontend y backend, así como de la
-                    gestión de la base de datos
-                  . La plataforma permite a administradores, ponentes y
-                  revisores gestionar, enviar y evaluar artículos de manera
-                  eficiente, optimizando el proceso de revisión y selección en
-                  eventos académicos.
-                </p>
+              <p className="experience-description">
+                Desarrollé Simponsi, un software de administración y control de
+                artículos en congresos científicos. Me encargué del diseño,
+                desarrollo frontend y backend, así como de la gestión de la base
+                de datos . La plataforma permite a administradores, ponentes y
+                revisores gestionar, enviar y evaluar artículos de manera
+                eficiente, optimizando el proceso de revisión y selección en
+                eventos académicos.
+              </p>
             </div>
           </div>
-
-
         </div>
       </div>
 
@@ -167,6 +169,51 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
         <div className="proyect-list">
           <div className="proyect">
             <div className="proyect-img">
+              <img src="/portfolio/simulandum.png" alt="" />
+            </div>
+
+            <div className="proyect-info">
+              <div className="proyect-info-top">
+                <h4>Simulandum</h4>
+
+                <div className="proyect-spans">
+                  <span>
+                    <img src="/icons/skills/nextjs.png"></img>
+                    <p>Next.js</p>
+                  </span>
+
+                  <span>
+                    <img src="/icons/skills/stripe.png"></img>
+                    <p>Stripe</p>
+                  </span>
+
+                  <span>
+                    <img src="/icons/skills/mongodb.png"></img>
+                    <p>MongoDB</p>
+                  </span>
+                </div>
+
+                <p>
+                  SaaS educativo para simulación de exámenes de admisión en
+                  México. Incluye pagos con Stripe, gestión de simuladores,
+                  preguntas, usuarios y un panel administrativo personalizado.{" "}
+                </p>
+              </div>
+              <div className="proyect-info-buttons">
+                <a
+                  className="bordered"
+                  href="https://toro-salgado.vercel.app"
+                  target="_blank"
+                >
+                  <img src="/icons/external-link.png"></img>
+                  LINK
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="proyect">
+            <div className="proyect-img">
               <img src="/portfolio/simposi.png" alt="" />
             </div>
 
@@ -180,18 +227,15 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
                     <p>React</p>
                   </span>
 
-
                   <span>
                     <img src="/icons/skills/nodejs.png"></img>
                     <p>Node.js</p>
                   </span>
 
-
                   <span className="third-tag">
                     <img src="/icons/skills/mysql.png"></img>
                     <p>MySQL</p>
                   </span>
-
                 </div>
 
                 <p>
@@ -201,15 +245,18 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
                 </p>
               </div>
               <div className="proyect-info-buttons">
-                <a href="https://github.com/guilebaldodev/PaperApp-Backend" target="_blank">
+                <a
+                  href="https://github.com/guilebaldodev/PaperApp-Backend"
+                  target="_blank"
+                >
                   <img src="/icons/github-white.png"></img>
                   GITHUB
                 </a>
 
-                <a className="bordered" href="">
+                {/* <a className="bordered" href="">
                   <img src="/icons/external-link.png"></img>
                   DEMO
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
@@ -242,12 +289,19 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
                 </p>
               </div>
               <div className="proyect-info-buttons">
-                <a href="https://github.com/guilebaldodev/Justicia-y-Progresismo" target="_blank">
+                <a
+                  href="https://github.com/guilebaldodev/Justicia-y-Progresismo"
+                  target="_blank"
+                >
                   <img src="/icons/github-white.png"></img>
                   GITHUB
                 </a>
 
-                <a className="bordered" href="https://www.justiciayprogresismo.com" target="_blank">
+                <a
+                  className="bordered"
+                  href="https://www.justiciayprogresismo.com"
+                  target="_blank"
+                >
                   <img src="/icons/external-link.png"></img>
                   LINK
                 </a>
@@ -288,12 +342,19 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
                 </p>
               </div>
               <div className="proyect-info-buttons">
-                <a href="https://github.com/guilebaldodev/ToroShow" target="_blank">
+                <a
+                  href="https://github.com/guilebaldodev/ToroShow"
+                  target="_blank"
+                >
                   <img src="/icons/github-white.png"></img>
                   GITHUB
                 </a>
 
-                <a className="bordered" href="https://github.com/guilebaldodev/ToroShow" target="_blank">
+                <a
+                  className="bordered"
+                  href="https://github.com/guilebaldodev/ToroShow"
+                  target="_blank"
+                >
                   <img src="/icons/external-link.png"></img>
                   DEMO
                 </a>
@@ -329,12 +390,19 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
                 </p>
               </div>
               <div className="proyect-info-buttons">
-                <a href="https://github.com/guilebaldodev/ToroSalgado" target="_blank">
+                <a
+                  href="https://github.com/guilebaldodev/ToroSalgado"
+                  target="_blank"
+                >
                   <img src="/icons/github-white.png"></img>
                   GITHUB
                 </a>
 
-                <a className="bordered" href="https://toro-salgado.vercel.app" target="_blank">
+                <a
+                  className="bordered"
+                  href="https://toro-salgado.vercel.app"
+                  target="_blank"
+                >
                   <img src="/icons/external-link.png"></img>
                   DEMO
                 </a>
@@ -347,26 +415,28 @@ Trabajo con Next.js, React y Node.js, manejando bases de datos SQL y NoSQL segú
       <div id="contact" className="section contact-section">
         <h2>Contáctame</h2>
         <p>
-        Especializado en <span>Desarrollo Full Stack</span>. Trabajemos juntos para hacer realidad tu proyecto.
+          Especializado en <span>Desarrollo Full Stack</span>. Trabajemos juntos
+          para hacer realidad tu proyecto.
         </p>
 
         <div className="contact-buttons">
-            <a href="https://www.linkedin.com/in/guilebaldo-vargas-788111223" target="_blank">
-              <img src="/icons/linkedin-black.png" alt="" />
-              <span>Linkedin</span>
-            </a>
-            <a href={whatsAppURL} target="_blank">
-              <img src="/icons/whatsapp-black.png" alt="" />
-              <span>WhatsApp</span>
-            </a>
-            {/* <a href="/cv.pdf">
+          <a
+            href="https://www.linkedin.com/in/guilebaldo-vargas-788111223"
+            target="_blank"
+          >
+            <img src="/icons/linkedin-black.png" alt="" />
+            <span>Linkedin</span>
+          </a>
+          <a href={whatsAppURL} target="_blank">
+            <img src="/icons/whatsapp-black.png" alt="" />
+            <span>WhatsApp</span>
+          </a>
+          {/* <a href="/cv.pdf">
               <img src="/icons/cv-black.png" alt="" />
               <span>Descargar cv</span>
             </a> */}
         </div>
-
       </div>
-      
     </>
   );
 }
